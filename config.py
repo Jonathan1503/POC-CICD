6277746dd18bf476c4e91fd0fefc8ba4087b117b
Jonathan Rivera <j.riverao@uniandes.edu.co>
2025-04-17 12:43:47 -0500

    Typo

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ else:
     DB_USER = os.getenv("DB_USER", "postgres")
     DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
     DB_HOST = os.getenv("DB_HOST", "localhost")
-    DB_PORT = os.getenv("DB_PORT", "5499")
+    DB_PORT = os.getenv("DB_PORT", "5432")
     DB_NAME = os.getenv("DB_NAME", "postgres")
 
     SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
