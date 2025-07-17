e14f004b44869d003e616c1bd74c49c154030a36
Jonathan RIVERA <jonathan.rivera@amadeus.com>
2025-07-17 09:33:36 -0500

    CD test3

--- application.py
+++ application.py
@@ -22,7 +22,7 @@ api.add_resource(BlacklistResource, "/blacklists", "/blacklists/<string:email>")
 # Endpoint de Healthcheck
 @application.route("/")
 def index():
-    return jsonify(status="test"), 200
+    return jsonify(status="test2"), 200
 
 with application.app_context():
     db.create_all()
